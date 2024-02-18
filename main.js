39a0af2c3a708a6ebfd499539547c9de980f05ea
Yannick Van den Brande <yavadebr@gmail.com>
2024-02-18 14:49:30 +0100

    feat: add questions

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const speakBtn = document.querySelector('#startButton');
+const speakBtn = document.querySelector('.startButton');
 const recognition = new SpeechRecognition();
 recognition.lang = 'en-US';
 
 const questions = [
-    "First question?",
-    "Second question?",
-    "Third question?"
+    "What is the capital of France?",
+    "How many continents are there in the world?",
+    "Which planet is known as the 'Red Planet'?",
+    "What is the capital city of Australia?",
+    "What is the approximate value of the mathematical constant e to 10 decimal places?"
 ];
+
+document.querySelector(".questionText").textContent = questions[0];
 
 recognition.onresult = function(event) {
     console.log(event);
     const transcript = event.results[0][0].transcript;
-    document.querySelector("#recognizedText").innerHTML = transcript;
+    document.querySelector(".recognizedText").textContent = "Recognized text: " + transcript;
     speakBtn.disabled = false;
 }
 
@@ -21,4 +25,3 @@ speakBtn.addEventListener('click', () => {
     recognition.start();
     speakBtn.disabled = true;
 });
-
